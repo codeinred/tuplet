8dc0ae372afb37c3352f2c5069161950a3305166
functionalperez <perez.cs@pm.me>
2020-09-24 16:54:18 -0600

    Check that a tuple is move-constructible

---
+++ test/initialize-test-3.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <string>
+#include <tuplet/tuplet.hpp>
+#include <memory>
+
+bool is_good(int a, int b, std::string c) {
+    return a == 1 && b == 2 && c == "Hello, world!";
+}
+
+int main() {
+    auto tup1 = tuplet::tuple{1, std::make_unique<int>(2), std::string("Hello, world!")};
+
+    auto tup = std::move(tup1);
+
+    static_assert(std::is_same_v<std::tuple_element_t<0 , decltype(tup)>, int>, "Expected int& at element 0");
+    static_assert(std::is_same_v<std::tuple_element_t<1 , decltype(tup)>, std::unique_ptr<int>>, "Expected int& at element 1");
+    static_assert(std::is_same_v<std::tuple_element_t<2 , decltype(tup)>, std::string>, "Expected std::string& at element 2");
+
+    return !is_good(
+        get<0>(tup),
+        *get<1>(tup),
+        get<2>(tup));
+}
