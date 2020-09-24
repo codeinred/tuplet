f8b233d94e6522ce793a0d8e8cb4a3fb3407488c
functionalperez <perez.cs@pm.me>
2020-09-24 16:50:27 -0600

    Check to ensure that moving a tuple moves the structured binding elements

---
+++ test/binding-test-3.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <string>
+#include <memory>
+#include <tuplet/tuplet.hpp>
+
+bool is_good(int a, int b, std::string c) {
+    return a == 1 && b == 2 && c == "Hello, world!";
+}
+
+int main() {
+    auto tup = tuplet::tuple{1, std::make_unique<int>(2) , std::string("Hello, world!")};
+
+    // Check that moving a tuple moves the elements when
+    // doing a structured bind
+    auto [a, b, c] = std::move(tup);
+
+    return !is_good(a, *b, c);
+}
