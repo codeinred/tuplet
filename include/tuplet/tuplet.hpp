860495153ee2920ff8faeff418f5a2853391aa29
functionalperez <perez.cs@pm.me>
2020-09-24 12:33:04 -0600

    First code commit

---
+++ include/tuplet/tuplet.hpp
@@ -0,0 +1,3 @@
+#include <type_traits>
+
+namespace tuplet {}
---
+++ test/hello-tuplet.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include <tuplet/tuplet.hpp>
+
+using namespace tuplet;
+int main() {
+
+    puts("hello, world!");
+}
