9d0b42c63370cf378f827a99f8136f45f7bc5c8f
functionalperez <perez.cs@pm.me>
2020-10-02 16:47:27 -0600

    Fixed missing check in test-nested-1.cpp

--- test/test-nested-1.cpp
+++ test/test-nested-1.cpp
@@ -8,9 +8,12 @@ bool is_good(int a, int b, std::string c) {
 int main() {
     using tuplet::tuple;
     using tuplet::pair;
+    using namespace tuplet::literals;
 
     auto x = tuple {
         tuple { pair { 1, 2 }},
         tuple { "Hello, world!" }
     };
+
+    return !is_good(x[0_idx][0_idx][0_idx], x[0_idx][0_idx][1_idx], x[1_idx][0_idx]);
 }
