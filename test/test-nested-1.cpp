8bc96e01f53a41252c7266904f5c2402ccc8be35
Alecto Irene Perez <perez.cs@pm.me>
2021-09-28 13:24:03 -0600

    Updated test/test-nested-1.cpp
    This commit updates the formatting of test/test-nested-1.cpp in
    accordance with the new clang-format file added in commit 53faad2
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- test/test-nested-1.cpp
+++ test/test-nested-1.cpp
@@ -13,5 +13,7 @@ int main() {
     auto x = tuple {tuple {pair {1, 2}}, tuple {"Hello, world!"}};
 
     return !is_good(
-        x[0_tag][0_tag][0_tag], x[0_tag][0_tag][1_tag], x[1_tag][0_tag]);
+        x[0_tag][0_tag][0_tag],
+        x[0_tag][0_tag][1_tag],
+        x[1_tag][0_tag]);
 }
