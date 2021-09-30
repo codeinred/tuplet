bd4112ab4ec7b194dc096818d7014b803c6c6784
Alecto Irene Perez <perez.cs@pm.me>
2021-09-29 20:56:11 -0600

    Wrote script to benchmark apply function
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

---
+++ bench-compile-times/bench-apply.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/zsh
+
+if [ -n "$CXX" ]; then
+    compiler="$CXX"
+else
+    compiler="g++"
+fi
+
+cpu_model="$(grep -m 1 "model name" /proc/cpuinfo)"
+compiler_version="$($compiler --version | head -n 1)"
+
+echo "Using $compiler aka $compiler_version"
+
+SKIP=$1
+MAX=$2
+
+echo -e "$cpu_model" "\n" "$compiler_version" "\n" "---" >data/tuplet-apply-times.csv
+echo -e "$cpu_model" "\n" "$compiler_version" "\n" "---" >data/std-apply-times.csv
+
+# Create print_ans.o
+$compiler -c print_ans.cpp
+
+for n in {$SKIP..$MAX..$SKIP}; do
+    # Create a list of n values from 1..n
+    values="$(echo {1..$n},)"
+    # This indicates that GNU time should be used, rather than the built-in shell time command
+    \time -f "$n, %e, %M" $compiler apply.cpp print_ans.o \
+        -std=c++20 \
+        -I../include \
+        -DVALUES="$values" 2>>data/tuplet-apply-times.csv
+
+    \time -f "$n, %e, %M" $compiler apply.cpp print_ans.o \
+        -std=c++20 \
+        -DVALUES="$values" \
+        -DUSE_STD_TUPLE 2>>data/std-apply-times.csv
+done
