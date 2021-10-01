2aebb35942dc2273ad8f19fb7d4e9b2722a770bc
Alecto Irene Perez <perez.cs@pm.me>
2021-10-01 02:14:58 -0600

    Expanded tests
    This commit expands the range of tests done in tuplet_cat to include
    tests concatenating greater numbers of tuples, and to ensure that move
    semantics work properly (catenating moved tuples should result in the
    elements being moved).
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- test/tuple-cat-test-1.cpp
+++ test/tuple-cat-test-1.cpp
@@ -1,8 +1,11 @@
 #include <cstdio>
+#include <memory>
+#include <string>
 #include <tuplet/tuple.hpp>
 
 using tuplet::tuple;
 using tuplet::tuple_cat;
+using tuplet::literals::operator""_tag;
 
 static_assert(
     std::is_same_v<decltype(tuple_cat()), tuple<>>,
@@ -23,7 +26,28 @@ static_assert(
 static_assert(
     std::is_same_v<
         decltype(tuple_cat(tuple<char>(), tuple<int>())),
-        tuple<char, int> >,
+        tuple<char, int>>,
     "tuple_cat broken");
 
-int main(){}
+static_assert(
+    std::is_same_v<
+        decltype(tuple_cat(tuple<char>(), tuple<int>(), tuple<std::string>())),
+        tuple<char, int, std::string>>,
+    "tuple_cat broken");
+
+int main() {
+
+    tuple<std::unique_ptr<int>, std::string, char, char, char> tup = tuple_cat(
+        tuple {std::make_unique<int>(69420)},
+        tuple {std::string {"Hello, world!"}},
+        tuple {'a'},
+        tuple {'b'},
+        tuple {'c'});
+
+    if (*tup[0_tag] == 69420 && tup[1_tag] == "Hello, world!"
+        && tup[2_tag] == 'a' && tup[3_tag] == 'b' && tup[4_tag] == 'c') {
+        return 0;
+    } else {
+        return 1;
+    }
+}
