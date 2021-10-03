9ae294fbd198bffc9ab269c69acb3b10f00af405
Alecto Irene Perez <perez.cs@pm.me>
2021-10-03 01:45:11 -0600

    Fixed formatting for empty tuple
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/tuplet/format.hpp
+++ include/tuplet/format.hpp
@@ -18,7 +18,7 @@ struct fmt::formatter<tuplet::tuple<T...>> {
             open_char = view[0];
             close_char = view[1];
         }
-        if(view.size() == 4) {
+        if (view.size() == 4) {
             open_char = view[0];
             separator = view[1];
             close_char = view[2];
@@ -28,12 +28,16 @@ struct fmt::formatter<tuplet::tuple<T...>> {
     template <typename FormatContext>
     constexpr auto format(const tuplet::tuple<T...>& p, FormatContext& ctx)
         -> decltype(ctx.out()) {
-        auto print_elems = [&](auto const& first, auto const&... rest) {
-            auto out = format_to(ctx.out(), "{}{}", open_char, first);
-            ((out = format_to(out, "{} {}", separator, rest)), ...);
-            return format_to(out, "{}", close_char);
-        };
-        return apply(print_elems, p);
+        if constexpr (sizeof...(T) >= 1) {
+            auto print_elems = [&](auto const& first, auto const&... rest) {
+                auto out = format_to(ctx.out(), "{}{}", open_char, first);
+                ((out = format_to(out, "{} {}", separator, rest)), ...);
+                return format_to(out, "{}", close_char);
+            };
+            return apply(print_elems, p);
+        } else {
+            return format_to(ctx.out(), "{}{}", open_char, close_char);
+        }
     }
 };
 #endif
