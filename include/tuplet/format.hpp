f3aaae9097300bc740f64b504a9352f5384869ec
Alecto Irene Perez <perez.cs@pm.me>
2021-10-02 23:00:01 -0600

    Added support for fmtlib
    This commit adds support for printing with fmtlib.
    
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

---
+++ include/tuplet/format.hpp
@@ -0,0 +1,36 @@
+#ifndef TUPLET_FMTLIB_HPP_IMPLEMENTATION
+#define TUPLET_FMTLIB_HPP_IMPLEMENTATION
+
+#include <fmt/format.h>
+#include <tuplet/tuple.hpp>
+
+template <class... T>
+struct fmt::formatter<tuplet::tuple<T...>> {
+    char open_char = '{';
+    char separator = ',';
+    char close_char = '}';
+    constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+        std::string_view view(ctx.begin(), ctx.end());
+        if (view.size() == 2) {
+            open_char = view[0];
+            close_char = view[1];
+        }
+        if(view.size() == 3) {
+            open_char = view[0];
+            separator = view[1];
+            close_char = view[2];
+        }
+        return ctx.begin() + view.size();
+    }
+    template <typename FormatContext>
+    constexpr auto format(const tuplet::tuple<T...>& p, FormatContext& ctx)
+        -> decltype(ctx.out()) {
+        auto print_elems = [&](auto const& first, auto const&... rest) {
+            auto out = format_to(ctx.out(), "{}{}", open_char, first);
+            ((out = format_to(out, "{} {}", separator, rest)), ...);
+            return format_to(out, "{}", close_char);
+        };
+        return apply(print_elems, p);
+    }
+};
+#endif
