5c03e4c13731563a13b6acff6b1d53ba10dee757
Alecto Irene Perez <perez.cs@pm.me>
2021-10-03 00:00:26 -0600

    Structured Binding Test w/ Sections 1-3
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- test2/main.cpp
+++ test2/main.cpp
@@ -98,3 +98,45 @@ TEST_CASE("Test Assignment") {
         REQUIRE(t2[2_tag] == "Hello, world!");
     }
 }
+TEST_CASE("Test structured bindings") {
+    SECTION("binding-1: Check tuple decomposition") {
+        auto tup = tuplet::tuple {1, 2, std::string("Hello, world!")};
+
+        auto [a, b, c] = tup;
+
+        REQUIRE(a == 1);
+        REQUIRE(b == 2);
+        REQUIRE(c == "Hello, world!");
+    }
+
+    SECTION("binding-2: Check tuple decomposition by reference") {
+        auto tup = tuplet::tuple {0, 0, std::string()};
+
+        auto& [a, b, c] = tup;
+
+        a = 1;
+        b = 2;
+        c = "Hello, world!";
+
+        REQUIRE(get<0>(tup) == 1);
+        REQUIRE(get<1>(tup) == 2);
+        REQUIRE(get<2>(tup) == "Hello, world!");
+    }
+
+    SECTION("binding-3: Check tuple decomposition by move") {
+        auto tup = tuplet::tuple {
+            1,
+            std::make_unique<int>(2),
+            std::string("Hello, world!")};
+
+        // Check that moving a tuple moves the elements when
+        // doing a structured bind
+        auto [a, b, c] = std::move(tup);
+
+        REQUIRE(a == 1);
+        REQUIRE(*b == 2);
+        REQUIRE(c == "Hello, world!");
+    }
+}
+
+
