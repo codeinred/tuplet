cd7b012a46f53dd3841cfbb57f9c80e035679efd
Alecto Irene Perez <perez.cs@pm.me>
2021-10-14 18:18:28 -0600

    Added check for format specification of tuple
    Signed-off-by: Alecto Irene Perez <perez.cs@pm.me>

--- include/tuplet/format.hpp
+++ include/tuplet/format.hpp
@@ -10,6 +10,7 @@ struct fmt::formatter<tuplet::tuple<T...>> {
     char separator = ',';
     char close_char = '}';
     constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+        using std::string_view_literals::operator""sv;
         std::string_view view(ctx.begin(), ctx.end() - ctx.begin());
         if (view.size() == 0) {
             return ctx.begin();
@@ -22,6 +23,14 @@ struct fmt::formatter<tuplet::tuple<T...>> {
             open_char = view[0];
             separator = view[1];
             close_char = view[2];
+        }
+        if ("<{[("sv.find(open_char) == std::string_view::npos) {
+            throw fmt::format_error(
+                fmt::format("Enable to interpret open char {}", open_char));
+        }
+        if (">}])"sv.find(close_char) == std::string_view::npos) {
+            throw fmt::format_error(
+                fmt::format("Unable to interpret close char {}", close_char));
         }
         return ctx.begin() + view.size() - 1;
     }
