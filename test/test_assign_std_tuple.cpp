1ae0879a4dc577fd815bf828656cb9a499641af5
Alecto Irene Perez <perez.cs@pm.me>
2022-12-01 22:00:25 -0700

    Fix test compilation

--- test/test_assign_std_tuple.cpp
+++ test/test_assign_std_tuple.cpp
@@ -1,62 +1,53 @@
 #include "util/printing.hpp"
 #include <catch2/catch_test_macros.hpp>
 #include <memory>
+#include <tuple>
 
 using namespace tuplet::literals;
 using tuplet::tuple;
 
 
-TEST_CASE("Check = operator for tuples", "[assign]") {
+TEST_CASE("Check = operator for tuples", "[assign-std-tuple]") {
     int a = 0;
     int b = 0;
     std::string c;
 
-    tuplet::tie(a, b, c) = tuplet::tuple {1, 2, std::string("Hello, world!")};
+    tuplet::tie(a, b, c) = std::tuple {1, 2, std::string("Hello, world!")};
 
     REQUIRE(a == 1);
     REQUIRE(b == 2);
     REQUIRE(c == "Hello, world!");
 }
 
-TEST_CASE("Operator = with heterogenous element types", "[assign]") {
+TEST_CASE("Operator = with heterogenous element types", "[assign-std-tuple]") {
     int a = 0;
     int b = 0;
     std::string c;
 
-    tuplet::tie(a, b, c) = tuplet::tuple {1, 2, "Hello, world!"};
+    tuplet::tie(a, b, c) = std::tuple {1, 2, "Hello, world!"};
 
     REQUIRE(a == 1);
     REQUIRE(b == 2);
     REQUIRE(c == "Hello, world!");
 }
 
-TEST_CASE("check assignment results in moves", "[assign]") {
-    tuplet::tuple<std::unique_ptr<int>, std::string> t, q;
-
-    t = {std::make_unique<int>(69420), "Hello, world!"};
-
-    q = std::move(t);
-
-    REQUIRE(t[0_tag].get() == nullptr);
-    REQUIRE(*q[0_tag] == 69420);
-    REQUIRE(q[1_tag] == "Hello, world!");
-}
-
-TEST_CASE("check assignment results in moves with tuplet::tie", "[assign]") {
+TEST_CASE(
+    "check assignment results in moves with tuplet::tie",
+    "[assign-std-tuple]") {
     std::shared_ptr<int> p;
     std::string s;
-    tuplet::tuple<std::shared_ptr<int>, std::string> t, q;
+    std::tuple<std::shared_ptr<int>, std::string> t, q;
 
     t = {std::make_shared<int>(69420), "Hello, world!"};
 
     tuplet::tie(p, s) = std::move(t);
 
-    REQUIRE(t[0_tag].get() == nullptr);
+    REQUIRE(std::get<0>(t) == nullptr);
     REQUIRE(*p == 69420);
     REQUIRE(s == "Hello, world!");
 }
 
-TEST_CASE("check tuple assignment", "[assign]") {
+TEST_CASE("check tuple assignment", "[assign-std-tuple]") {
     using namespace tuplet::literals;
     std::tuple<int, int, std::string> t1 {1, 2, "Hello, world!"};
     tuplet::tuple<int, int, std::string> t2;
@@ -66,18 +57,4 @@ TEST_CASE("check tuple assignment", "[assign]") {
     REQUIRE(t2[0_tag] == 1);
     REQUIRE(t2[1_tag] == 2);
     REQUIRE(t2[2_tag] == "Hello, world!");
-}
-
-TEST_CASE("Assignment to empty tuple", "[assign]") {
-    tuplet::tuple<> empty_tuple;
-
-    // A tuple should be assignable from a stateless type
-    empty_tuple = [] {};
-
-    // A empty tuple should be reference assignable from a stateless type
-    auto empty_lambda = [] {};
-    empty_tuple = empty_lambda;
-
-    // Checks that self-assignment compiles
-    empty_tuple = empty_tuple;
 }
